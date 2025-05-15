578a5d15617f98ab8fa349b60cd57e8dc6e238a7
Mariusz Mańka <mariusz.manka@3qcode.com>
2025-05-16 00:19:47 +0200

    adding better logging

--- backend/src/DatabaseConnector.py
+++ backend/src/DatabaseConnector.py
@@ -4,9 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 from src.GlobalConfig import settings
 
+import logging
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
 
 # ======================== DATABASE INITIALIZATION & CONFIG ========================
-engine = create_engine(str(settings.SQL_DATABASE_URL))
+engine = create_engine(str(settings.SQL_DATABASE_URL), echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 # ==================================================================================
